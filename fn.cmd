4d60203fbb4fefab6d557f7315bc5ef16b8d77b9
Leonardo Farias Bona <leonardofariasbona@gmail.com>
2025-03-15 16:28:22 -0300

    RIP

--- fn.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-title Searching files with %*
-set folder=%2
-if defined folder set folder=-path %folder%
-es %folder% %1 | fzf --bind "enter:execute(nvim {})" 
--- hosts.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-call adm %0
-nvim C:\Windows\System32\drivers\etc\hosts
